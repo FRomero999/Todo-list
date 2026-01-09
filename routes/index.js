a5e282c9dfdad12eef35885fc664e92787bae418
Francisco <you@example.com>
2026-01-09 14:23:08 +0100

    Add SQLite database integration for task management; create tasks table and insert sample data; update admin route to fetch tasks

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var router = express.Router();
 var authMiddleware = require('../middlewares/auth');
+var Database = require('better-sqlite3');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', {});
+});
+
 
 /* GET login page. */
 router.get('/login', function(req, res, next) {
@@ -22,8 +29,23 @@ router.post('/login', function(req, res, next) {
   }
 });
 
+/* GET logout page. */
+router.get('/logout', function(req, res, next) {
+  req.session.user = null;
+  res.redirect('/');
+});
+
 /* GET admin page. */
 router.get('/admin', authMiddleware, function(req, res, next) {
+  
+  /* hacer una consulta a la base de datos */
+
+  let db = new Database("db.sqlite");
+  const sql = `select * from tareas`;
+  let salida = db.prepare(sql).all();
+
+  console.log(salida);
+
   res.render('admin', { title: 'Express', user: req.session.user, layout: 'layout-admin' });
 });
 
